787d61b9c7080054a7955608eaf8daf25312f915
Daniel Freitas Ferreira <72029912+Daniel-f-ferreira@users.noreply.github.com>
2021-12-17 18:39:48 +0000

    Update Sex.java

--- src/linearalgebra/model/Sex.java
+++ src/linearalgebra/model/Sex.java
@@ -3,8 +3,10 @@ package linearalgebra.model;
 public enum Sex {
     MALE(1, "Male"), 
     FEMALE(2, "Female");
+   
     
-    // attribute that matches the first option of the enum: 1 or 2
+    
+    
     private final int code;
     // attribute that corresponds to the second option of the enum: Male ou Female
     private final String description;
