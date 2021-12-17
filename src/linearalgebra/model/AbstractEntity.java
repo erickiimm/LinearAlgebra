3b09b596c0190926ad535d7b49949498e4c16794
Daniel Freitas Ferreira <72029912+Daniel-f-ferreira@users.noreply.github.com>
2021-12-17 18:32:09 +0000

    Update AbstractEntity.java

--- src/linearalgebra/model/AbstractEntity.java
+++ src/linearalgebra/model/AbstractEntity.java
@@ -1,17 +1,6 @@
 package linearalgebra.model;
 
-/* 
-- Entity Abstract Class
-- An abstract class cannot be instantiated
-- All classes that extend this class will have access to the public attributes.
-- It was created to reuse the id attribute in all classes that are persisted in the database.
-- Also in order to separate the model from data persistence...
 
-Visibility:
-- public: everyone accesses this information
-- protected: only the parent class and the child class access this attribute.
-- private: only the class itself accesses this attribute.
-*/
 public abstract class AbstractEntity {
     
     private Long id;
